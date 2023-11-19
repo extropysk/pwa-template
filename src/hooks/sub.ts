0a38b133b6c243414cf800e8c64e879e69ebd9f2
Kristián Duda <kristian.duda@extropy.sk>
2023-11-19 16:42:54 +0100

    add service worker

---
+++ src/hooks/sub.ts
@@ -0,0 +1,52 @@
+import { getEnv } from "@/utils/config";
+import { useEffect, useState } from "react";
+
+export function useSub() {
+  const [permission, setPermission] = useState<NotificationPermission>(
+    Notification.permission
+  );
+  const [subscription, setSubscription] = useState<PushSubscription | null>();
+
+  useEffect(() => {
+    (async () => {
+      const reg = await navigator.serviceWorker.getRegistration();
+      const subscription = await reg?.pushManager.getSubscription();
+      setSubscription(subscription);
+    })();
+  }, []);
+
+  const unsubscribe = async () => {
+    const reg = await navigator.serviceWorker.getRegistration();
+    const subscription = await reg?.pushManager.getSubscription();
+    await subscription?.unsubscribe();
+    setSubscription(null);
+  };
+
+  const subscribe = async () => {
+    if (permission === "denied") return;
+
+    const reg = await navigator.serviceWorker.getRegistration();
+    let subscription = await reg?.pushManager.getSubscription();
+
+    if (subscription) return;
+
+    if (permission === "default") {
+      const p = await window.Notification.requestPermission();
+      setPermission(p);
+      if (p === "denied") return;
+    }
+
+    subscription = await reg?.pushManager.subscribe({
+      userVisibleOnly: true,
+      applicationServerKey: getEnv("VITE_VAPID_PUBLIC"),
+    });
+    setSubscription(subscription);
+  };
+
+  return {
+    permission,
+    subscribe,
+    unsubscribe,
+    subscription,
+  };
+}
--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,28 @@
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
 import { defineConfig } from "vite";
+import { VitePWA } from "vite-plugin-pwa";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePWA({
+      srcDir: "src",
+      registerType: "autoUpdate",
+      filename: "sw.js",
+      strategies: "injectManifest",
+      injectRegister: "auto",
+      manifest: false,
+      injectManifest: {
+        injectionPoint: null,
+      },
+      devOptions: {
+        enabled: true,
+        type: "module",
+      },
+    }),
+  ],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
